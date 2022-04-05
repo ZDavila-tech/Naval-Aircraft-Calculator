fc1fcb9062a539e3136f7dc9b7c55305915f8ecf
ZDavila-tech <zoleda.davila@students.tamuk.edu>
2022-04-05 15:42:17 -0500

    Trying to fix app crash

--- app/src/main/java/com/example/full_sail_project/MainActivity.java
+++ app/src/main/java/com/example/full_sail_project/MainActivity.java
@@ -22,14 +22,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     TextView refuel;
     TextView travel;
     Spinner spinner;
-    static double answ, time, speed, refueltime,range;
+    static double answ;
+    static double time;
+    static double speed;
+    static double refueltime;
+    static double range;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
         inputlat1 = findViewById(R.id.lat1);
         inputlat2 = findViewById(R.id.lat2);
         inputlong1 = findViewById(R.id.long1);
@@ -38,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         refuel = findViewById(R.id.refuel);
         travel = findViewById(R.id.travel);
         spinner = findViewById(R.id.spinner_aircraft);
-        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this, R.array.aircraft, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.aircraft, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
@@ -61,176 +65,176 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case 0:
                 break;
             case 1:
-                speed = 132;
-                range = 1259;
+                speed = 132.0;
+                range = 1259.0;
                 break;
             case 2:
-                speed = 145;
-                range = 1269;
+                speed = 145.0;
+                range = 1269.0;
                 break;
             case 3:
-                speed = 278;
-                range = 2917;
+                speed = 278.0;
+                range = 2917.0;
                 break;
             case 4:
-                speed = 289;
-                range = 6186;
+                speed = 289.0;
+                range = 6186.0;
                 break;
             case 5:
-                speed = 270;
-                range = 4530;
+                speed = 270.0;
+                range = 4530.0;
                 break;
             case 6:
-                speed = 280;
-                range = 2428;
+                speed = 280.0;
+                range = 2428.0;
                 break;
             case 7:
-                speed = 275;
-                range = 6667;
+                speed = 275.0;
+                range = 6667.0;
                 break;
             case 8:
-                speed = 275;
-                range = 3013;
+                speed = 275.0;
+                range = 3013.0;
                 break;
             case 9:
-                speed = 106;
-                range = 889;
+                speed = 106.0;
+                range = 889.0;
                 break;
             case 10:
-                speed = 114;
-                range = 765;
+                speed = 114.0;
+                range = 765.0;
                 break;
             case 11:
-                speed = 1034;
-                range = 3704;
+                speed = 1034.0;
+                range = 3704.0;
                 break;
             case 12:
-                speed = 560;
-                range = 2385;
+                speed = 560.0;
+                range = 2385.0;
                 break;
             case 13:
-                speed = 478;
-                range = 12816;
+                speed = 478.0;
+                range = 12816.0;
                 break;
             case 14:
-                speed = 473;
-                range = 7749;
+                speed = 473.0;
+                range = 7749.0;
                 break;
             case 15:
-                speed = 524;
-                range = 22409;
+                speed = 524.0;
+                range = 22409.0;
                 break;
             case 16:
-                speed = 1026;
-                range = 4345;
+                speed = 1026.0;
+                range = 4345.0;
                 break;
             case 17:
-                speed = 1032;
-                range = 2028;
+                speed = 1032.0;
+                range = 2028.0;
                 break;
             case 18:
-                speed = 426;
-                range = 4115;
+                speed = 426.0;
+                range = 4115.0;
                 break;
             case 19:
-                speed = 138;
-                range = 1356;
+                speed = 138.0;
+                range = 1356.0;
                 break;
             case 20:
-                speed = 139;
-                range = 2815;
+                speed = 139.0;
+                range = 2815.0;
                 break;
             case 21:
-                speed = 248;
-                range = 4815;
+                speed = 248.0;
+                range = 4815.0;
                 break;
             case 22:
-                speed = 288;
-                range = 4445;
+                speed = 288.0;
+                range = 4445.0;
                 break;
             case 23:
-                speed = 505;
-                range = 12742;
+                speed = 505.0;
+                range = 12742.0;
                 break;
             case 24:
-                speed = 508;
-                range = 23150;
+                speed = 508.0;
+                range = 23150.0;
                 break;
             case 25:
-                speed = 108;
-                range = 343;
+                speed = 108.0;
+                range = 343.0;
                 break;
             case 26:
-                speed = 154;
-                range = 1769;
+                speed = 154.0;
+                range = 1769.0;
                 break;
             case 27:
-                speed = 152;
-                range = 1565;
+                speed = 152.0;
+                range = 1565.0;
                 break;
             case 28:
-                speed = 324;
-                range = 6667;
+                speed = 324.0;
+                range = 6667.0;
                 break;
             case 29:
-                speed = 1145;
-                range = 7815;
+                speed = 1145.0;
+                range = 7815.0;
                 break;
             case 30:
-                speed = 1042;
-                range = 4111;
+                speed = 1042.0;
+                range = 4111.0;
                 break;
             case 31:
-                speed = 405;
-                range = 16668;
+                speed = 405.0;
+                range = 16668.0;
                 break;
             case 32:
-                speed = 936;
-                range = 4599;
+                speed = 936.0;
+                range = 4599.0;
                 break;
             case 33:
-                speed = 140;
-                range = 1111;
+                speed = 140.0;
+                range = 1111.0;
                 break;
             case 34:
-                speed = 326;
-                range = 4784;
+                speed = 326.0;
+                range = 4784.0;
                 break;
             case 35:
-                speed = 351;
-                range = 5019;
+                speed = 351.0;
+                range = 5019.0;
                 break;
             case 36:
-                speed = 310;
-                range = 28123;
+                speed = 310.0;
+                range = 28123.0;
                 break;
             case 37:
-                speed = 115;
-                range = 2778;
+                speed = 115.0;
+                range = 2778.0;
                 break;
             case 38:
-                speed = 745;
-                range = 3398;
+                speed = 745.0;
+                range = 3398.0;
                 break;
             case 39:
-                speed = 170;
-                range = 3843;
+                speed = 170.0;
+                range = 3843.0;
                 break;
             case 40:
-                speed = 180;
-                range = 2408;
+                speed = 180.0;
+                range = 2408.0;
                 break;
             case 41:
-                speed = 180;
-                range = 4111;
+                speed = 180.0;
+                range = 4111.0;
                 break;
             case 42:
-                speed = 178;
-                range = 1304;
+                speed = 178.0;
+                range = 1304.0;
                 break;
             case 43:
-                speed = 159;
-                range = 1082;
+                speed = 159.0;
+                range = 1082.0;
                 break;
         }
     }
@@ -240,7 +244,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    private void calDistance(){
+    public void calDistance(){
         double conlat1 = Double.parseDouble(inputlat1.getText().toString());
         double conlat2 = Double.parseDouble(inputlat2.getText().toString());
         double conlong1 = Double.parseDouble(inputlong1.getText().toString());
@@ -257,17 +261,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 * Math.pow(Math.sin(dlon / 2), 2);
         double c = 2 * Math.asin(Math.sqrt(a));
         double r = 6371;
-         answ = c * r;
-        distance.setText("You are "+ Double.toString(answ) + " kilometers away from your specified destination");
+         answ = c * r * 0.868976;
+         String result = "You are "+ answ + " nautical miles away from your specified destination";
+        distance.setText(result);
 
     }
-    private void calTime(){
+    public void calTime(){
         time = answ / speed;
-        travel.setText("It will take " + Double.toString(time) + " hours to reach your specified destination at maximum speed");
+        String first = "It will take " + time + " hours at maximum speed";
+        travel.setText(first);
     }
-    private void calRefuel(){
+    public void calRefuel(){
         refueltime = answ / range;
-        refuel.setText("You will need to refuel: " + Double.toString(refueltime) + " times at maximum speed");
+        String result = "You will need to refuel: " + refueltime + " times at maximum speed";
+        refuel.setText(result);
     }
 
 }
